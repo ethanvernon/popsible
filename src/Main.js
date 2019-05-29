33183b846d27c955528b69c155e9bb36e61ce1fb
Ethan Vernon <ethan.vernon@colorado.edu>
2019-05-29 17:40:49 +0800

    adds full-screen carousel background to main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export class Main extends Component {
 
@@ -13,13 +16,41 @@ export class Main extends Component {
 
 		//this.handleCopy = this.handleCopy.bind(this);
 	}
-	
+
 	render() {
+		var settings = {
+			dots: true,
+			infinite: true,
+			speed: 7000,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			fade:true,
+			pauseOnHover: false
+		};
 		return (
-			<div className='main' id='main'>
-				<div className='statement'>
+			<Slider {...settings} className='main-slider'>
+				<div className='main main-img-1' id='main'>
 				</div>
-			</div>
+				<div className='main main-img-2' id='main'>
+				</div>
+				<div className='main main-img-3' id='main'>
+				</div>
+				<div className='main main-img-4' id='main'>
+				</div>
+			</Slider>
 		);
 	}
 }
+
+
+	
+	// render() {
+	// 	return (
+	// 		<div className='main' id='main'>
+	// 			<div className='statement'>
+	// 			</div>
+	// 		</div>
+			
+	// 	);
+	// }
