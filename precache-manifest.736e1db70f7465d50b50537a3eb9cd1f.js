82164b6f127da3cea94ed351896af7ccdffb75f1
Ethan Vernon <ethan.vernon@colorado.edu>
2019-05-30 20:33:12 +0800

    Updates

---
+++ precache-manifest.736e1db70f7465d50b50537a3eb9cd1f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11036401c605e62835a68fb35d5c018",
+    "url": "/lolli-take-home/index.html"
+  },
+  {
+    "revision": "bdc4ab5b0be9d15507ed",
+    "url": "/lolli-take-home/static/css/2.bb904a91.chunk.css"
+  },
+  {
+    "revision": "ac55c67bc6ed748a1803",
+    "url": "/lolli-take-home/static/css/main.860cb919.chunk.css"
+  },
+  {
+    "revision": "bdc4ab5b0be9d15507ed",
+    "url": "/lolli-take-home/static/js/2.673810e9.chunk.js"
+  },
+  {
+    "revision": "ac55c67bc6ed748a1803",
+    "url": "/lolli-take-home/static/js/main.da62cf26.chunk.js"
+  },
+  {
+    "revision": "5bcf4caf5043853b2eba",
+    "url": "/lolli-take-home/static/js/runtime~main.09faece9.js"
+  },
+  {
+    "revision": "a082ee69f50e15bf2a166bae339b14ea",
+    "url": "/lolli-take-home/static/media/coffee.a082ee69.jpg"
+  },
+  {
+    "revision": "61913fb8182d8637f9c7e71efa49b855",
+    "url": "/lolli-take-home/static/media/concert.61913fb8.jpg"
+  },
+  {
+    "revision": "1c6baccd097ca73796b4546c14aa95cd",
+    "url": "/lolli-take-home/static/media/eating.1c6baccd.jpg"
+  },
+  {
+    "revision": "5f4d24d2e216ef63e074fdc65ee6ab22",
+    "url": "/lolli-take-home/static/media/footer-background.5f4d24d2.jpg"
+  },
+  {
+    "revision": "41362a6368ccc8122822093ffd4ad2d3",
+    "url": "/lolli-take-home/static/media/happy-people.41362a63.jpg"
+  },
+  {
+    "revision": "3922342ce51abadc6ecf904f7f90fe4f",
+    "url": "/lolli-take-home/static/media/home-back.3922342c.jpg"
+  },
+  {
+    "revision": "9db143ec9aa4648046a97503c762c711",
+    "url": "/lolli-take-home/static/media/home-back2.9db143ec.jpg"
+  },
+  {
+    "revision": "aa3ccd9db1444ebe3c25dd38aaf73184",
+    "url": "/lolli-take-home/static/media/home-back3.aa3ccd9d.jpg"
+  },
+  {
+    "revision": "281f6b522fbd4fe74c346fb3880fa939",
+    "url": "/lolli-take-home/static/media/home-back4.281f6b52.jpg"
+  },
+  {
+    "revision": "6b11f7f41a5962e857892dee3a1423c6",
+    "url": "/lolli-take-home/static/media/hotel-slide1.6b11f7f4.jpg"
+  },
+  {
+    "revision": "29e7ebf235d740a64b097afbfa5cba58",
+    "url": "/lolli-take-home/static/media/mockup.29e7ebf2.jpg"
+  },
+  {
+    "revision": "1a81a7a25e4160e0422c624bd9833379",
+    "url": "/lolli-take-home/static/media/pool.1a81a7a2.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lolli-take-home/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lolli-take-home/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lolli-take-home/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lolli-take-home/static/media/slick.f97e3bbf.svg"
+  }
+]);
