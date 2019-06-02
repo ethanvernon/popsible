03aa1bf980316047d592c574fdfa29dea9bb9ddc
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-02 19:31:55 +0800

    add pop-out from horizontal for navbar

--- src/Header.js
+++ src/Header.js
@@ -19,8 +19,11 @@ export class Header extends Component {
 			isOpen: false,
 			width: 0,
 			height: 0,
+			scroll: 0,
 			navClass: 'invis-nav',
-			containerClass: 'invis-container'
+			containerClass: 'invis-container',
+			showNav: false,
+			darkFontOnly: false
 		};
 
 		this.toggle = this.toggle.bind(this);
@@ -45,8 +48,40 @@ export class Header extends Component {
 	}
 
 	toggle() {
+
+		let newClass;
+		let newContainerClass;
+		let darkFonts;
+
+		//if we're closing nav and we're past the top of page
+		if (this.state.scroll > this.state.height-100 && this.state.showNav) {
+			newClass='solid-nav';
+			newContainerClass='solid-container';
+			darkFonts=true;
+		}
+
+		//if we're closing nav and we're not past top of page
+		if (this.state.scroll < this.state.height-100 && this.state.showNav) {
+			newClass='invis-nav';
+			newContainerClass='invis-container';
+			this.setState({
+				navClass: newClass,
+				containerClass: newContainerClass
+			});
+			darkFonts=false;
+		}
+
+		//if we're opening the nav
+		if (!this.state.showNav) {
+			newClass='solid-nav';
+			newContainerClass='solid-container';	
+			darkFonts=true		
+		}
+
 		this.setState({
-			isOpen: !this.state.isOpen
+			isOpen: !this.state.isOpen,
+			showNav: !this.state.showNav,
+			darkFontOnly: darkFonts
 		});
 	}
 
@@ -61,44 +96,78 @@ export class Header extends Component {
 		let newClass=this.state.navClass;
 		let newContainerClass=this.state.containerClass;
 
-		console.log(scroll);
+		//console.log(scroll);
 		console.log(this.state.height);
 
+		//we're past the top and the nav is open
 		if (scroll > this.state.height-100) {
 			newClass='solid-nav';
 			newContainerClass='solid-container';
-		} else {
+		} else if (scroll < this.state.height-100) {
+			newClass='invis-nav';
+			newContainerClass='invis-container';
+		} else if (this.state.showNav) {
+			newClass='solid-nav';
+			newContainerClass='solid-nav';
+		} else if (scroll > this.state.height-100 && !this.state.showNav) { //we're at the top and the nav is closed
 			newClass='invis-nav';
 			newContainerClass='invis-container';
 		}
 
 		this.setState({
 			navClass: newClass,
-			containerClass: newContainerClass
+			containerClass: newContainerClass,
+			scroll: scroll
 		});
+
+		console.log(this.state.scroll);
 	}
 
 
-	render() {
-	return (
-		<div className={'navbar-container '+ this.state.containerClass} onScroll={e => this.handleScroll(e)}>
-			<Navbar className={this.state.navClass} >
-				<NavbarBrand href="/" className="mr-auto"><img src={textLogo}/></NavbarBrand>
-				<Nav navbar className='ml-auto book-now'>
-				<NavItem>
-					book now
-				</NavItem>
-				</Nav>
+	render() {		
+
+		let showNav='';
+		let darkFont='';
+
+		if (this.state.showNav) {
+			showNav='show';
+			darkFont='dark-nav-items';
+		}
+
+		return (
+
+			<div className={'navbar-container '+ this.state.containerClass} onScroll={e => this.handleScroll(e)}>
+				<Navbar className={this.state.navClass} >
+					<NavbarBrand href="/" className="mr-auto"><img src={textLogo}/></NavbarBrand>
+					<Nav navbar className={'ml-auto book-now '+darkFont}>
+					<NavItem>
+						book now
+					</NavItem>
+					</Nav>
+					<div onClick={this.toggle} className={'navbar-toggler-icon '+darkFont} >
+					</div>
+				</Navbar>
+
+
+
+						<div className={'collapse-menu-container '+showNav}>
+							<ul className='collapse-list'>
+								<li className='collapse-list-item'>Navlink</li>
+								<li className='collapse-list-item'>Navlink</li>
+								<li className='collapse-list-item'>Navlink</li>
+							</ul>
+						</div>
+			</div>
+		);
+	}
+}
+/*
+
 				<NavbarToggler onClick={this.toggle} className='mr-2'/>
 				<Collapse isOpen={this.state.isOpen} navbar>
-					<Nav navbar className='ml-auto'>
+					<Nav navbar horizontal='right' className='ml-auto'>
 						<NavItem>
 							About
 						</NavItem>
 					</Nav>
-				</Collapse>
-			</Navbar>
-		</div>
-    );
-  }
-}
+				</Collapse>*/
