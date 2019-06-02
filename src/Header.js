e0fd8c18e98c66b82f7b8a69cb7fb1b1ecaa3675
Ethan Vernon <ethan.vernon@colorado.edu>
2019-06-02 13:36:34 +0800

    makes navbar fade to white and inverts colors on scroll

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import textLogo from './anthead-logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import {
   Navbar,
   NavbarBrand,
@@ -9,19 +9,40 @@ import {
   NavItem,
   Collapse,
   NavbarToggler} from 'reactstrap';
+import { Controller, Scene } from 'react-scrollmagic';
 
 export class Header extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-		isOpen: false
+			isOpen: false,
+			width: 0,
+			height: 0,
+			navClass: 'invis-nav',
+			containerClass: 'invis-container'
 		};
 
 		this.toggle = this.toggle.bind(this);
 		this.closeNav = this.closeNav.bind(this);
+		this.handleScroll = this.handleScroll.bind(this);
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 	}
 
+	componentDidMount() {
+		this.updateWindowDimensions();
+		window.addEventListener('scroll', this.handleScroll);
+		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+
+	componentWillUnmount(){
+		window.removeEventListener('scroll', this.handleScroll);
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+
+	updateWindowDimensions() {
+		this.setState({ width: window.innerWidth, height: window.innerHeight });
+	}
 
 	toggle() {
 		this.setState({
@@ -35,11 +56,33 @@ export class Header extends Component {
 		}
 	}
 
+	handleScroll(e) {
+		let scroll=parseInt(e.target.scrollingElement.scrollTop);
+		let newClass=this.state.navClass;
+		let newContainerClass=this.state.containerClass;
+
+		console.log(scroll);
+		console.log(this.state.height);
+
+		if (scroll > this.state.height-100) {
+			newClass='solid-nav';
+			newContainerClass='solid-container';
+		} else {
+			newClass='invis-nav';
+			newContainerClass='invis-container';
+		}
+
+		this.setState({
+			navClass: newClass,
+			containerClass: newContainerClass
+		});
+	}
+
 
 	render() {
 	return (
-		<div className='navbar-container'>
-			<Navbar id='white-nav' color='light' light>
+		<div className={'navbar-container '+ this.state.containerClass} onScroll={e => this.handleScroll(e)}>
+			<Navbar className={this.state.navClass} >
 				<NavbarBrand href="/" className="mr-auto"><img src={textLogo}/></NavbarBrand>
 				<Nav navbar className='ml-auto book-now'>
 				<NavItem>
